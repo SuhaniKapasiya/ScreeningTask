17dcbc7e33773c41e3f78bb9e5af66d285071579
SuhaniKapasiya <107610361+SuhaniKapasiya@users.noreply.github.com>
2025-03-06 16:29:24 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+
+const PORT = 5000;
+
+// API Endpoint
+app.get("/api/greet", (req, res) => {
+
+  const name = req.query.name;
+ 
+  
+  if (!name) {
+    return res.status(400).json({ error: "Name is required." });
+  }
+  res.json({ message: `Hello, ${name}! Welcome to Younglabs.` });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
